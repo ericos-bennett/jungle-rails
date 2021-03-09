0ed8748ce7b501528a6245e0436eee9e68102642
Eric <ericos.bennett@gmail.com>
2021-03-09 02:09:27 +0000

    changed name attr in user table to first_name and last_name

---
+++ db/migrate/20210309015957_split_user_name.rb
@@ -0,0 +1,9 @@
+class SplitUserName < ActiveRecord::Migration
+  def change
+
+    remove_column :users, :name
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+
+  end
+end
